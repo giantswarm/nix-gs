e960d9e12d9bfdb08d7799330d7a194fb88bb185
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2023-11-29 11:06:22 +0100

    [opsctl] use vendorHash

--- lib/opsctl.nix
+++ lib/opsctl.nix
@@ -5,7 +5,7 @@ buildGoModule {
   pname = "opctl";
   version = "5.3.0";
 
-  vendorSha256 = "sha256-QOhBXvvvMkLv1GMQzLzGikLilPN/qF7unM02955veEk=";
+  vendorHash = "sha256-QOhBXvvvMkLv1GMQzLzGikLilPN/qF7unM02955veEk=";
 
   CGO_ENABLED = 0;
 
