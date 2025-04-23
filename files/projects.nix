b15017169ff5543da50875d46222060713b03ce7
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2025-04-23 09:56:41 +0200

    [projects] add roadmap project

--- files/projects.nix
+++ files/projects.nix
@@ -1,6 +1,15 @@
 let
   workDir = "work/giantswarm";
   gitlabRepo = "git.tools.kbee.xyz";
+
+  gsProject = name: {
+    name = "gs-${name}";
+    path = "${workDir}/gs-${name}";
+    repos = [{
+      name = "origin";
+      url = "git@github.com:giantswarm/${name}.git";
+    }];
+  };
 in [
   (rec {
     name = "nix-gs";
@@ -16,4 +25,5 @@ in [
       }
     ];
   })
+  (gsProject "roadmap")
 ]
