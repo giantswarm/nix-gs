d516fdd35eedbc6389a7481e360b025c31f04b67
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2025-07-02 14:56:13 +0200

    update muster to v0.0.9

--- lib/muster.nix
+++ lib/muster.nix
@@ -1,16 +1,16 @@
 { pkgs, buildGoModule }:
 buildGoModule rec {
   pname = "muster";
-  version = "0.0.7";
+  version = "0.0.9";
 
   src = pkgs.fetchFromGitHub {
     owner = "giantswarm";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-YKaulQbYkzpyT1DIUFOVXdpX9zQLwhlWxjd+HgGrVsQ=";
+    hash = "sha256-eGvwiX2/yPrY5Ak7kn8th8uadyIv62Bu5EMryYzmniM=";
   };
 
-  vendorHash = "sha256-35o7Wwp/L7OggAf8JWhL/kRAXAJuyPnSfFJolkpaDCo=";
+  vendorHash = "sha256-GCi5x7TAGXY7iaIQ1wxuEFRHZA066rWTmE45eskHdfM=";
 
   doCheck = false;
 
