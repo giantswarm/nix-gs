48a38776698a865005afcc61bc419330b812d3cc
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2025-10-16 09:51:08 +0200

    update kubectl-gs to v4.8.1

--- lib/kubectl-gs.nix
+++ lib/kubectl-gs.nix
@@ -1,16 +1,16 @@
 { pkgs, buildGoModule }:
 buildGoModule rec {
   pname = "kubectl-gs";
-  version = "4.8.0";
+  version = "4.8.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "giantswarm";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-DWAIYX0r8U/wR8broKlSCn9Pl8/o1moWDpkVKjktXRQ=";
+    hash = "sha256-YtlqmM6dSKI3QIW7cE0wwoDY1L5ciNqyHUwpoUFJgJQ=";
   };
 
-  vendorHash = "sha256-VtbDi9WhVudcjB4XbUY9h6KKYiFafIf0itvxoWlxBZM=";
+  vendorHash = "sha256-OY8Khe9nCLukpLluZXCuTPdynUc3bN9ig5Zm9qJ9tfk=";
 
   env.CGO_ENABLED = 0;
 
